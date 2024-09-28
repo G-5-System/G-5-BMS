29c51005da970ce1e7b922999a779076fa64086b
Rossy <rosswellacabo2004@gmail.com>
2024-09-28 22:27:02 +0800

    Add project files.

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace G_5_BMS.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View(); 
+        }
+    }
+}
+
---
+++ Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace G_5_BMS.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult AdminAuth()
+        {
+            return View();
+        }
+    }
+}
